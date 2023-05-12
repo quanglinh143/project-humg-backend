97fb1c163a459aa0d70c301872d8eaa41747ba6f
q.linh <punkdrug143@gmail.com>
2023-05-12 21:58:19 +0700

    fix: rf - token, create payment by admin

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -62,7 +62,7 @@ const login = async (req, res) => {
   const refreshtoken = createRefreshToken({ id: user._id })
   res.cookie("refreshtoken", refreshtoken, {
    secure: true,
-   domain: "https://chamm.onrender.com/",
+   domain: "chamm.onrender.com/",
    path: "/",
    maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
   })
