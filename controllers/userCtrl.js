cf7fcbd3005d2dfaede04ea027190258e63a235c
q.linh <punkdrug143@gmail.com>
2023-05-13 13:14:05 +0700

    update ctrl

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -63,6 +63,7 @@ const login = async (req, res) => {
   res.cookie("refreshtoken", refreshtoken, {
    maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
   })
+  res.cookie("name", "geeksforgeeks")
 
   return res.json({ accesstoken })
  } catch (error) {
