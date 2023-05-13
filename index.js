a0223e1dd1b8ec46ba02bf8be9f0af2bde9cc640
q.linh <punkdrug143@gmail.com>
2023-05-13 13:04:51 +0700

    update origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 const corsOptions = {
- origin: "https://project-humg-backend.vercel.app",
+ origin: "http://localhost:3000",
  credentials: true, //access-control-allow-credentials:true
  optionSuccessStatus: 200,
 }
