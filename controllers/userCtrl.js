b00d5717eb8eddc1064efcb3128210cfa96f8468
q.linh <punkdrug143@gmail.com>
2023-05-14 09:06:04 +0700

    upd ctrl user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -106,7 +106,7 @@ const loginDashboard = async (req, res) => {
 
 const logout = async (req, res) => {
  try {
-  res.clearCookie("refreshtoken", { path: "/" })
+  res.clearCookie("refreshtoken")
   res.json({ msg: "Logged out!" })
  } catch (error) {
   return res.status(500).json({ msg: error.message })
